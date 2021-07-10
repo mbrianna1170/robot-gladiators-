e5bf804388ab6c601a234e2319e38f4c3edaac93
mbrianna1170 <brianna.miller09@yahoo.com>
2021-07-10 01:19:55 -0500

    created first function for game

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let playerName = window.prompt('What is your robots name?');
+// Displays playerName back to user
+console.log('Introducing ' + playerName + '!');
+
+function fight () {
+    window.alert('The fight has started!');
+}
+
+// fight();
